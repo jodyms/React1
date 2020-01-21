d259be8cd4385c190500f72194974174ea2f37c2
unknown <jodydiva@gmail.com>
2020-01-22 03:16:37 +0700

    Updates

---
+++ precache-manifest.89fb33d864bdb1ea0d5c1a9c377c86a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ec131bbd59a8ede8b149b598c0a7c50",
+    "url": "/React1/index.html"
+  },
+  {
+    "revision": "eeba3987ffbb3772110f",
+    "url": "/React1/static/css/main.ef65e47d.chunk.css"
+  },
+  {
+    "revision": "d706f28003964d78c4b2",
+    "url": "/React1/static/js/2.1339efe9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/React1/static/js/2.1339efe9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "eeba3987ffbb3772110f",
+    "url": "/React1/static/js/main.18c84bdf.chunk.js"
+  },
+  {
+    "revision": "c0b20179b57b2a64213c",
+    "url": "/React1/static/js/runtime-main.3cc371b6.js"
+  }
+]);
